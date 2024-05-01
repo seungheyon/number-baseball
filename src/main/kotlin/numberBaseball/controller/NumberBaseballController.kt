21d566117c0e7d4175a259589216b3c5483b4896
LSH <fhzktm@naver.com>
2024-05-01 21:03:42 +0900

    Feat : 입력 메세지 순서 변경

--- src/main/kotlin/numberBaseball/controller/NumberBaseballController.kt
+++ src/main/kotlin/numberBaseball/controller/NumberBaseballController.kt
@@ -10,18 +10,21 @@ class NumberBaseballController(
     private val randomNumberGenerator: RandomNumberGenerator
 ) {
     fun start() {
+        println("숫자야구를 시작합니다. ")
         val randomNumber = randomNumberGenerator.generateRandomNumber(
             NumberBaseballEnum.GameSize.GAMESIZE.size,
             NumberBaseballEnum.RandomNumber.RANDOMNUMBERFROM.number,
             NumberBaseballEnum.RandomNumber.RANDOMNUMBERUNTIL.number
         )
-        while (numberProcessor.processGameNumber(parseInputToIntList(),randomNumber))
+        while (numberProcessor.processGameNumber(parseInputToIntList(),randomNumber)){
+
+        }
         println("3개의 숫자를 모두 맞히셨습니다! 게임을 종료합니다.");
     }
 
     private fun parseInputToIntList(): MutableList<Int> {
         while (true) {
-            print("숫자야구를 시작합니다. ")
+
             print("${NumberBaseballEnum.RandomNumber.RANDOMNUMBERFROM.number} ~ ${NumberBaseballEnum.RandomNumber.RANDOMNUMBERUNTIL.number-1}")
             println(" 사이의 ${NumberBaseballEnum.GameSize.GAMESIZE.size}자리 수를 입력해 주세요.")
             try {
